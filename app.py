630e4f5e10cf30b31865ee58319e9107516c2643
dasew <vladislav.grand@gmail.com>
2017-06-14 20:51:52 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def wsgi_app(environ, start_response):
             for i in range(0,len(x)-1):
                 x[i]=float(x[i])
             y=calc(x)
-            response_body="Samsung Galaxy A3: "+str(y[0])+"<br>"+"Samsung Galaxy A5: "+str(y[1])+"<br>"+"Samsung Galaxy S7: "+str(y[2])+"<br>"+"Samsung Galaxy S8: "+str(y[3])+"<br>"+"iPhone6S: "+str(y[4])+"<br>"+"iPhone7: "+str(y[5])+"<br>"+"Lenovo Vibe K5 Note: "+str(y[6])+"<br>"+"Lenovo K6 Note: "+str(y[7])+"<br>"+"Lenovo Moto X Style: "+str(y[8])+"<br>"+"Lenovo Mote Z: "+str(y[9])+"<br>"
+            response_body="Воспользоваться услугами биржевого брокера: "+str(y[0])+"<br>"+"Использовать техничсекий анализ: "+str(y[1])+"<br>"+"Использовать функциональный анализ: "+str(y[2])+"<br>"+"Прогнощы на основе новостей: "+str(y[3])+"<br>"+"iPhone6S: "+str(y[4])+"<br>"+"iPhone7: "+str(y[5])+"<br>"+"Комбинация фундаментального и технического анализа: "+str(y[6])+"<br>"+"Комбинация фундаментального анализа и новостей: "+str(y[7])+"<br>"+"Комбинация технического анализа и новостей: "+str(y[8])+"<br>"+"Комбинация фундаментального,технического анализа и новостей: "+str(y[9])+"<br>"
             start_response(status, response_headers)
             yield response_body.encode()
         except:
